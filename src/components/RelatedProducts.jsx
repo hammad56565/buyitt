80f342614c1410f3b4c7bb6dd649e9dd1c3484b2
Muhammad Hammad <mhammad936666@gmail.com>
2025-06-29 20:42:28 +0500

    cappital

--- src/components/RelatedProducts.jsx
+++ src/components/RelatedProducts.jsx
@@ -17,8 +17,9 @@ const RelatedProducts = ({category,subCategory}) => {
     }, [products]);
   return (
     <div className='my-24'>
-      <div className=" text-center text-3xl py-2">
+      <div className=" text-center texhereforpush text-3xl py-2">
         <Title text1={'RELATED'} text2={'PRODUCTS'} />
+        {/* hlo hamma here */}
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
         {relatedProducts.map((item,index) => (
